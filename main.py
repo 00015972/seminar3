b1b9119dff07c104d6262f54f8e36dabdb3e2706
Mirsodiq <15972wiut@gmail.com>
2023-09-28 12:34:49 +0500

    init: this is my first version of project

---
+++ main.py
@@ -0,0 +1,8 @@
+#task5
+# decimal_number = input("please type any number")
+# decimal_to_binary = bin(int(decimal_number))
+# print("this is a binary number: " + decimal_to_binary)
+
+decimal_number = input("please type any number")
+decimal_to_octal = oct(int(decimal_number))
+print("this is a octal number: " + decimal_to_octal)
